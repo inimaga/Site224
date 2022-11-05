c3e0ad86343881cec394a020df9ebe645d2b438b
Issa Nimaga <issa.nimaga@gmail.com>
2022-11-05 19:05:54 +0000

    Updated API ahead of support for pagination on the front-end

--- Web Application/site224api/src/main/java/com/fabudable/site224api/Controllers/ApiController.java
+++ Web Application/site224api/src/main/java/com/fabudable/site224api/Controllers/ApiController.java
@@ -6,6 +6,7 @@ import javax.validation.Valid;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.*;
 
 import com.fabudable.site224api.Models.Location;
@@ -28,43 +29,46 @@ public class ApiController {
     @RequestMapping(method = RequestMethod.GET, path = "/locations")
     public List<Location> getLocations(@RequestParam(value = "city", defaultValue = "") String city,
                                        @RequestParam(value = "parentCategory", defaultValue = "") String parentCategory,
-                                       @RequestParam(value = "category", defaultValue = "") String category) {
+                                       @RequestParam(value = "category", defaultValue = "") String category,
+                                       @RequestParam(value = "page", defaultValue = "0") int pageNumber
+                                       ) {
+        int resultsPerPage = 10;
                                         
         if (!city.isEmpty() && !parentCategory.isEmpty() && !category.isEmpty()) {
-            return locationInterface.findByCityAndParentCategoryAndCategory(city, parentCategory, category);
+            return locationInterface.findByCityAndParentCategoryAndCategory(city, parentCategory, category, PageRequest.of(pageNumber, resultsPerPage));
         } else if (!city.isEmpty() && !parentCategory.isEmpty()) {
-            return locationInterface.findByCityAndParentCategory(city, parentCategory);
+            return locationInterface.findByCityAndParentCategory(city, parentCategory, PageRequest.of(pageNumber, resultsPerPage));
         } else if (!parentCategory.isEmpty() && !category.isEmpty()) {
-            return locationInterface.findByParentCategoryAndCategory(parentCategory, category);
+            return locationInterface.findByParentCategoryAndCategory(parentCategory, category, PageRequest.of(pageNumber, resultsPerPage));
         } else if (!city.isEmpty() && !category.isEmpty()) {
-            return locationInterface.findByCityAndCategory(city, category);
+            return locationInterface.findByCityAndCategory(city, category, PageRequest.of(pageNumber, resultsPerPage));
         } else if (!city.isEmpty()) {
-            return locationInterface.findByCity(city);
+            return locationInterface.findByCity(city, PageRequest.of(pageNumber, resultsPerPage));
         } else if (!parentCategory.isEmpty()) {
-            return locationInterface.findByParentCategory(parentCategory);
+            return locationInterface.findByParentCategory(parentCategory, PageRequest.of(pageNumber, resultsPerPage));
         } else if (!category.isEmpty()) {
-            return locationInterface.findByCategory(category);
+            return locationInterface.findByCategory(category, PageRequest.of(pageNumber, resultsPerPage));
         }
 
-        return locationInterface.findAll();
+        return locationInterface.findAll(PageRequest.of(pageNumber, resultsPerPage)).getContent();
     }
 
     //Endpoint to get locations by city
     @RequestMapping(method = RequestMethod.GET, path = "/locations/{city}")
     public List<Location> getLocationsByCity(@PathVariable String city) {
-        return locationInterface.findByCity(city);
+        return locationInterface.findByCity(city, PageRequest.of(0, 5));
     }
 
     //Endpoint to get locations by parent category
     @RequestMapping(method = RequestMethod.GET, path = "/locations/parentCategory/{parentCategory}")
     public List<Location> getLocationsByParentCategory(@PathVariable String parentCategory) {
-        return locationInterface.findByParentCategory(parentCategory);
+        return locationInterface.findByParentCategory(parentCategory, PageRequest.of(0, 100));
     }
 
     //Endpoint to get locations by category
     @RequestMapping(method = RequestMethod.GET, path = "/locations/category/{category}")
     public List<Location> getLocationsByCategory(@PathVariable String category) {
-        return locationInterface.findByCategory(category);
+        return locationInterface.findByCategory(category, PageRequest.of(0, 100));
     }
 
 
--- Web Application/site224api/src/main/java/com/fabudable/site224api/Interface/LocationInterface.java
+++ Web Application/site224api/src/main/java/com/fabudable/site224api/Interface/LocationInterface.java
@@ -4,21 +4,24 @@ import com.fabudable.site224api.Models.Location;
 
 import java.util.List;
 
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LocationInterface extends JpaRepository<Location, Long> {
     
-    public List<Location> findByCity(String city);
+    public List<Location> findByCity(String city, PageRequest pageRequest);
 
-    public List<Location> findByParentCategory(String parentCategory);
+    public List<Location> findByParentCategory(String parentCategory, PageRequest pageRequest);
 
-    public List<Location> findByCategory(String category);
+    public List<Location> findByCategory(String category, PageRequest pageRequest);
 
-    public List<Location> findByCityAndParentCategoryAndCategory(String city, String parentCategory, String category);
+    public List<Location> findByCityAndParentCategoryAndCategory(String city, String parentCategory, String category, PageRequest pageRequest);
 
-    public List<Location> findByCityAndParentCategory(String city, String parentCategory);
+    public List<Location> findByCityAndParentCategory(String city, String parentCategory, PageRequest pageRequest);
 
-    public List<Location> findByParentCategoryAndCategory(String parentCategory, String category);
+    public List<Location> findByParentCategoryAndCategory(String parentCategory, String category, PageRequest pageRequest);
 
-    public List<Location> findByCityAndCategory(String city, String category);
+    public List<Location> findByCityAndCategory(String city, String category, PageRequest pageRequest);
+
 }
